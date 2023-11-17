2a2a93e8d02d4310be66860a12e94a3795181ded
DanilaChubarov124 <danilcubarov4810@gmail.com>
2023-11-17 23:24:45 +0300

    Danya

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_11_17_134628) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
